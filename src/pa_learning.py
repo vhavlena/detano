2a1fc5ea7cb94748774988276d097148b70ea473
vhavlena <vohavlena@gmail.com>
2021-10-08 14:53:39 +0200

    learning: parameters

--- src/pa_learning.py
+++ src/pa_learning.py
@@ -20,10 +20,12 @@ If not, see <http://www.gnu.org/licenses/>.
 """
 
 import sys
+import getopt
 import os
 import csv
 import math
 from enum import Enum
+from dataclasses import dataclass
 
 import learning.fpt as fpt
 import learning.alergia as alergia
@@ -31,14 +33,32 @@ import parser.IEC104_parser as con_par
 import parser.IEC104_conv_parser as iec_prep_par
 
 rows_filter = ["asduType", "cot"]
-TRAINING = 0.33
+TRAINING = 1.0
 
 """
 Program parameters
 """
-class Params(Enum):
+class Algorithms(Enum):
+    PA = 0
+    PTA = 1
+
+
+"""
+Program parameters
+"""
+class InputFormat(Enum):
     IPFIX = 0
     CONV = 1
+
+
+"""
+Program parameters
+"""
+@dataclass
+class Params:
+    alg : Algorithms
+    file : str
+    file_format : InputFormat
 
 
 """
@@ -57,63 +77,136 @@ def print_help():
 
 
 """
-Main
+Function for learning based on Alergia (PA)
 """
-def main():
-    argc = len(sys.argv)
-
-    if argc < 2:
-        sys.stderr.write("Error: bad parameters\n")
-        print_help()
-        sys.exit(1)
-
-    ptype = Params.IPFIX
-    csv_file = sys.argv[1]
-    if argc == 3 and sys.argv[2] == "--conv":
-        ptype = Params.CONV
-
-    csv_fd = open(csv_file, "r")
-    csv_file = os.path.basename(csv_file)
-
-    ############################################################################
-    # Preparing the learning data
-    ############################################################################
-    normal_msgs = con_par.get_messages(csv_fd)
-
-    parser = None
-    if ptype == Params.IPFIX:
-        parser = con_par.IEC104Parser(normal_msgs)
-    elif ptype == Params.CONV:
-        parser = iec_prep_par.IEC104ConvParser(normal_msgs)
-
-    parser.parse_conversations()
-    lines = parser.get_all_conversations(abstraction)
-
-    index = int(len(lines)*TRAINING)
-    training, testing = lines[:index], lines[index:]
+def learn_pa(training):
+    if len(training) == 0:
+        raise Exception("training set is empty")
 
     tree = fpt.FPT()
     for line in training:
         tree.add_string(line)
 
     alpha = 0.05
-    t0 = int(math.log(index, 2))
+    t0 = int(math.log(len(training), 2))
 
     aut = alergia.alergia(tree, alpha, t0)
     aut.rename_states()
-    fa = aut.normalize()
+    return aut.normalize(), alpha, t0
 
+
+"""
+Function for learning based on prefix trees (PTA)
+"""
+def learn_pta(training):
+    if len(training) == 0:
+        raise Exception("training set is empty")
+
+    tree = fpt.FPT()
+    tree.add_string_list(training)
+    tree.rename_states()
+    return tree.normalize(), None, None
+
+
+"""
+Store automaton into file
+"""
+def store_automata(csv_file, fa, alpha, t0):
     store_filename = os.path.splitext(os.path.basename(csv_file))[0]
-    store_filename = "{0}a{1}t{2}".format(store_filename, alpha, t0)
+    if (alpha is not None) and (t0 is not None):
+        store_filename = "{0}a{1}t{2}".format(store_filename, alpha, t0)
+    else:
+        store_filename = "{0}-pta".format(store_filename)
 
     fa_fd = open("{0}.fa".format(store_filename), "w")
     fa_fd.write(fa.to_fa_format(True))
     fa_fd.close()
 
-    legend = "File: {0}, alpha: {1}, t0: {2}".format(csv_file, alpha, t0)
+    if (alpha is not None) and (t0 is not None):
+        legend = "File: {0}, alpha: {1}, t0: {2}".format(csv_file, alpha, t0)
+    else:
+        legend = "File: {0}".format(csv_file)
     dot_fd = open("{0}.dot".format(store_filename), "w")
     dot_fd.write(fa.to_dot(aggregate=False, legend=legend))
     dot_fd.close()
+
+
+"""
+Main
+"""
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ht:f:", ["help", "type=", "format="])
+        if len(args) > 0:
+            opts, _ = getopt.getopt(args[1:], "ht:f:", ["help", "type=", "format="])
+    except getopt.GetoptError as err:
+        sys.stderr.write("Error: bad parameters\n")
+        print_help()
+        sys.exit(1)
+
+    params = Params(Algorithms.PA, None, InputFormat.IPFIX)
+    learn_fnc = learn_pa
+
+    for o, a in opts:
+        if o in ("-t", "--type"):
+            if a == "pa":
+                params.alg = Algorithms.PA
+                learn_fnc = learn_pa
+            elif a == "pta":
+                params.alg = Algorithms.PTA
+                learn_fnc = learn_pta
+        elif o in ("-h", "--help"):
+            print_help()
+            sys.exit()
+        elif o in ("-f", "--format"):
+            if a == "conv":
+                params.file_format = InputFormat.CONV
+            elif a == "ipfix":
+                params.file_format = InputFormat.IPFIX
+        else:
+            sys.stderr.write("Error: unrecognized parameters\n")
+            sys.exit(1)
+
+    if len(args) == 0:
+        sys.stderr.write("Missing input file\n")
+        sys.exit(1)
+    params.file = args[0]
+
+    try:
+        csv_fd = open(params.file, "r")
+    except FileNotFoundError:
+        sys.stderr.write("Cannot open file: {0}\n".format(params.file))
+        sys.exit(1)
+    csv_file = os.path.basename(params.file)
+
+    ############################################################################
+    # Preparing the learning data
+    ############################################################################
+    normal_msgs = con_par.get_messages(csv_fd)
+    csv_fd.close()
+
+    parser = None
+    try:
+        if params.file_format == InputFormat.IPFIX:
+            parser = con_par.IEC104Parser(normal_msgs)
+        elif params.file_format == InputFormat.CONV:
+            parser = iec_prep_par.IEC104ConvParser(normal_msgs)
+        parser.parse_conversations()
+    except KeyError as e:
+        sys.stderr.write("Missing column in the input csv: {0}\n".format(e))
+        sys.exit(1)
+
+    lines = parser.get_all_conversations(abstraction)
+    index = int(len(lines)*TRAINING)
+    training, testing = lines[:index], lines[index:]
+
+    try:
+        fa, alpha, t0 = learn_fnc(training)
+    except Exception as e:
+        sys.stderr.write("Learning error: {0}\n".format(e))
+        sys.exit(1)
+
+    store_automata(csv_file, fa, alpha, t0)
 
     miss = 0
     for line in testing:
@@ -122,13 +215,12 @@ def main():
             miss += 1
 
     print("File: {0}".format(csv_file))
-    print("alpha: {0}, t0: {1}".format(alpha, t0))
-    print("States {0}".format(len(aut.get_states())))
+    if (alpha is not None) and (t0 is not None):
+        print("alpha: {0}, t0: {1}".format(alpha, t0))
+    print("States {0}".format(len(fa.get_states())))
     print("Testing: {0}/{1} (missclassified/all)".format(miss, len(testing)))
     if len(testing) > 0:
         print("Accuracy: {0}".format((len(testing)-miss)/float(len(testing))))
-
-    csv_fd.close()
 
 
 if __name__ == "__main__":
